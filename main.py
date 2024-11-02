e332aa5cd0a3d0e62e92df0e2fae60d1f81f6ea0
yusufberk <138680780+yusufber@users.noreply.github.com>
2024-11-02 21:06:58 +0300

    main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+
+meme_dict = {
+            "CRINGE": "Garip ya da utandırıcı bir şey",
+            "LOL": "Komik bir şeye verilen cevap",
+            }
+word = input("Anlamadığınız bir kelime yazın (hepsini büyük harflerle yazın!): ")
+if word in meme_dict.keys():
+    print(word + ":"+ meme_dict[word])
+else:
+    print("yanlış girdiniz")
